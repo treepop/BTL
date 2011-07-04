de0cad836e3968a0cce0dd37ac286bc2feeda02f
superjoe <treepop.sunpetchniyom@nectec.or.th>
2011-07-04 17:04:52 +0700

    Create project for Bee the Lion

---
+++ src/com/img/jk/beethelion/mainActivity.java
@@ -0,0 +1,13 @@
+package com.img.jk.beethelion;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class mainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
