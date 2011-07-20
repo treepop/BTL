96097eb9159dae7e8de32aa2db485edd98144960
superjoe <treepop.sunpetchniyom@nectec.or.th>
2011-07-20 22:05:38 +0700

    takes a photo then writes it to SD card.

--- src/com/img/jk/beethelion/mainActivity.java
+++ src/com/img/jk/beethelion/mainActivity.java
@@ -1,14 +1,36 @@
-/* Bug that has been corrected yet.
- * 7 July 54 preview image was wrong orientation but when it was corrected,
- * take picture button was wrong orientation instead.
- * This bug was solved.
+/* This program is used to identify type of flower via photo.
+ * User takes a photo of unknown flower then this program list top 5 most likely.
+ * 
+ * Written at 2554-7-4 by Rungkarn Siricharoenchai Project manager.
+ * 						  Treepop Sunpetchniyom	   TU Master.
  *  
- * 7 July 54 I think macro wasn't effect.
+ * Version 1.3
+ * Platform Android
+ *  
+ * History
+ * -------
+ * 1.0 Original.
+ * 1.1 7 July 54 preview image was wrong orientation but when it was corrected,
+ * the take picture button was wrong orientation instead.
+ * This bug was solved.
+ * 1.2 7 July 54 I think macro wasn't effect.
+ * 1.3 20 July 54 Write file to SD CARD.
+ * 
+ * Future work
+ * -----------
+ * The saved Image file was still wrong orientation.
+ * 
+ * Abbreviation for any comment.
+ * 
  */
 
 package com.img.jk.beethelion;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 import android.app.Activity;
@@ -18,6 +40,7 @@ import android.hardware.Camera.PictureCallback;
 import android.hardware.Camera.ShutterCallback;
 import android.hardware.Camera.Size;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
@@ -58,15 +81,16 @@ public class mainActivity extends Activity implements SurfaceHolder.Callback {
         takePictureBtn.setOnClickListener(new OnClickListener() {
         	@Override
         	public void onClick(View v) {
-				 x10Camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+				 x10Camera.takePicture(previewCallback, rawCallback, jpegCallback);
 			}
 		});
-        /*This line has bug. The screen has wrong obj orientation.
+        
+        /*This line has bug. The screen has wrong object orientation.
         This command doesn't correct the orientation bug.
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);*/
     }
     
-    ShutterCallback shutterCallback = new ShutterCallback() {
+    ShutterCallback previewCallback = new ShutterCallback() {
 		
 		@Override
 		public void onShutter() {}
@@ -84,13 +108,58 @@ public class mainActivity extends Activity implements SurfaceHolder.Callback {
 		public void onPictureTaken(byte[] data, Camera camera) {
 			if(data != null) {
 				jpegData = data;
+				String fNameUnknowFlower = "unknowFlower.jpg";
+				File sdDir = Environment.getExternalStorageDirectory();
+				if(sdDir.exists()) {
+					if(sdDir.canWrite()) {
+						File beeDir = new File(sdDir.getAbsolutePath()+"/beeTheLion");
+						if(!beeDir.exists()) {
+							beeDir.mkdir();
+						}
+						if(beeDir.exists() && beeDir.canWrite()) {
+							File fileUnknowFlower = new File(beeDir.getAbsolutePath()+
+									"/"+fNameUnknowFlower);
+							try {
+								fileUnknowFlower.createNewFile();
+							} catch(IOException e) {
+								Log.v("jkError", "Can't create file unknowFlower.jpg");
+							}
+							if(fileUnknowFlower.exists() &&
+									fileUnknowFlower.canWrite()) {
+								FileOutputStream fos = null;
+								try {
+									fos = new FileOutputStream(fileUnknowFlower);
+									fos.write(jpegData);
+								} catch(FileNotFoundException e) {
+									
+								} catch(IOException e) {
+									
+								}
+								finally {
+									if(fos != null) {
+										try {
+											fos.flush();
+											fos.close();
+										} catch (IOException e) {
+											// swallow
+										}
+									}
+								}
+							}
+						}
+					} else {
+						// SD CARD can't write!
+					}
+				} else {
+					// SD CARD not found!
+				}
 				done();
 			}
 		}
 	};
 	
 	void done() {
-		finish();
+		finish(); // Exit program.
 	}
 	
 	@Override
@@ -104,12 +173,14 @@ public class mainActivity extends Activity implements SurfaceHolder.Callback {
         	x10Parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_MACRO);
         
         // Set picture size.
-        /* Don't know how to check whether there is 640.
         List<Camera.Size> x10PicSizeMode = x10Parameters.getSupportedPictureSizes();
-        if(x10PicSizeMode.contains(640))
-        	x10Parameters.setPictureSize(640, 480);
-    	*/
-        x10Parameters.setPictureSize(3264, 2448);
+        Iterator<Camera.Size> iterPicSizeMode = x10PicSizeMode.iterator();
+        while(iterPicSizeMode.hasNext()) {
+        	Camera.Size item = iterPicSizeMode.next();
+        	if(item.width == 640 && item.height == 480) {
+        		x10Parameters.setPictureSize(640, 480);
+        	}
+        }
         
         // Set preview frame rate.
         List<Integer> x10PreviewFrameRate = x10Parameters
@@ -119,7 +190,14 @@ public class mainActivity extends Activity implements SurfaceHolder.Callback {
         
         // Set preview size.
         List<Camera.Size> x10PreSizeMode = x10Parameters.getSupportedPreviewSizes();
-        x10Parameters.setPreviewSize(1280, 720);
+        Iterator<Camera.Size> iterPreSizeMode = x10PreSizeMode.iterator();
+        while(iterPreSizeMode.hasNext()) {
+        	Camera.Size item = iterPreSizeMode.next();
+        	if(item.width == 640 && item.height == 480) {
+        		x10Parameters.setPreviewSize(640, 480);
+        	}
+        }
+        
         
         // Fix bug wrong orientation.
         if(this.getResources().getConfiguration().orientation !=
@@ -158,20 +236,21 @@ public class mainActivity extends Activity implements SurfaceHolder.Callback {
         x10Parameters = x10Camera.getParameters();
         
         String infoFocusMode = x10Parameters.getFocusMode();
-        Toast.makeText(this, infoFocusMode, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Focus = " + infoFocusMode, Toast.LENGTH_SHORT).show();
         Size infoPictureSize = x10Parameters.getPictureSize();
         Integer w = infoPictureSize.width;
         Integer h = infoPictureSize.height;
         String strPictureSize = w.toString() + " x " + h.toString();
-        Toast.makeText(this, strPictureSize, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Pic size = " + strPictureSize, Toast.LENGTH_SHORT).show();
         Integer infoPreviewFrameRate = x10Parameters.getPreviewFrameRate();
-        Toast.makeText(this, infoPreviewFrameRate.toString(),
+        Toast.makeText(this, "Frame rate = " + infoPreviewFrameRate.toString(),
         		Toast.LENGTH_SHORT).show();
         Size infoPreviewSize = x10Parameters.getPreviewSize();
         w = infoPreviewSize.width;
         h = infoPreviewSize.height;
         String strPreviewSize = w.toString() + " x " + h.toString();
-        Toast.makeText(this, strPreviewSize, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Preview size = " + strPreviewSize, Toast.LENGTH_SHORT)
+        	.show();
         // ========= 
 	}
 	
